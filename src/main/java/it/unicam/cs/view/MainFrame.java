dfaecac754b875327babf020e6320b7888a5aa05
DazeDC <riccardo.coltrinari@studenti.unicam.it>
2021-03-19 18:08:54 +0100

    Added GUI with swing, added uncovered and empty png

---
+++ src/main/java/it/unicam/cs/view/MainFrame.java
@@ -0,0 +1,17 @@
+package it.unicam.cs.view;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MainFrame(String title) {
+		super(title);
+		
+		
+	}
+}
