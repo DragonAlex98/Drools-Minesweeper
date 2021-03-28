a318fe68c19a7f45ea55abc1a1f1090eadc5da54
Marco <marco.scarpetta@studenti.unicam.it>
2021-03-28 12:21:24 +0200

    Added solver by step, Mainframe & Solver refactor

--- src/main/java/it/unicam/cs/solver/Solver.java
+++ src/main/java/it/unicam/cs/solver/Solver.java
@@ -9,66 +9,89 @@ import it.unicam.cs.model.Grid;
 import it.unicam.cs.model.Location;
 import it.unicam.cs.model.Number;
 
+/**
+ * Class used to represent a Minesweeper Solver that uses the Single Point Strategy.
+ *
+ */
 public class Solver {
 
+	/**	Grid of used by the Solver to solve the game**/
 	private Grid grid;
 
 	public Solver(Grid grid) {
 		this.grid = grid;
 	}
 
+	/**
+	 * Method to check all mine neighbors (AMN) and possibly flag all of them.
+	 */
 	private void firstStep() {
 		grid.getGridAsStream().filter(s -> s.getState() == SquareState.UNCOVERED && s.getType() == SquareType.NUMBER).forEach(s -> {
 			long neighbours = grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.FLAGGED || n.getState() == SquareState.COVERED).count();
 			if (neighbours == ((Number)s).getNeighbourBombsCount()) {
 				grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.COVERED).forEach(n -> {
-					DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup("FLAG").setFocus();
-					DroolsUtils.getInstance().getKSession().insert(n.getLocation());
-					DroolsUtils.getInstance().getKSession().fireAllRules();
+					insertAndFire("FLAG", n.getLocation());
 				});
 			}
 		});
 	}
-
+	
+	/**
+	 * Method to check all free neighbors (AFN) and possibly uncover all of them.
+	 */
 	private void secondStep() {
 		grid.getGridAsStream().filter(s -> s.getState() == SquareState.UNCOVERED && s.getType() == SquareType.NUMBER).forEach(s -> {
 			long neighbours = grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.FLAGGED).count();
 			if (neighbours == ((Number)s).getNeighbourBombsCount()) {
 				grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.COVERED).forEach(n -> {
-					DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup("UNCOVER").setFocus();
-					DroolsUtils.getInstance().getKSession().insert(n.getLocation());
-					DroolsUtils.getInstance().getKSession().fireAllRules();
+					insertAndFire("UNCOVER", n.getLocation());
 				});
 			}
 		});
 	}
 
-	public void solve() {
-		if (!grid.isPopulated()) {
+	/**
+	 * Method to solve the Minesweeper game using the Single Point Strategy.
+	 * 
+	 * @param isByStep True if the Solver should perform only one resolution step, False otherwise.
+	 */
+	public void solve(boolean isByStep) {
+		if (!grid.isPopulated()) { // populate if not
 			Location randomLocation = grid.getRandomPoint();
 			grid.populateSafeGrid(randomLocation);
-			DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup("UNCOVER").setFocus();
-			DroolsUtils.getInstance().getKSession().insert(randomLocation);
-			DroolsUtils.getInstance().getKSession().fireAllRules();
+			insertAndFire("UNCOVER", randomLocation);
+			if(isByStep) {
+				return;
+			}
 		}
 		if (grid.getGameState() != GameState.ONGOING) {
 			return;
 		}
 		do {
 			String oldGrid = grid.toString();
-			firstStep();
-			secondStep();
+			firstStep();  // AMN
+			secondStep(); // AFN
 			String newGrid = grid.toString();
 			if (oldGrid.equals(newGrid)) {
 				Location randomLocation;
-				do {
+				do {  // if no action is performed, select random Covered Square
 					randomLocation = grid.getRandomPoint();
 				} while(grid.getSquareAt(randomLocation).getState() != SquareState.COVERED);
-				DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup("UNCOVER").setFocus();
-				DroolsUtils.getInstance().getKSession().insert(randomLocation);
-				DroolsUtils.getInstance().getKSession().fireAllRules();
+				insertAndFire("UNCOVER", randomLocation);
 			}
-		} while(grid.getGameState() == GameState.ONGOING);
+		} while(!isByStep && grid.getGameState() == GameState.ONGOING);
+	}
+	
+	/**
+	 * Method to insert an Object into the working memory and fire the rules corresponding to its agendaGroup.
+	 * 
+	 * @param agendaGroup The agendGroup to use to fire the rules.
+	 * @param object The object to insert into the working memory.
+	 */
+	private void insertAndFire(String agendaGroup, Object object) {
+		DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup(agendaGroup).setFocus();
+		DroolsUtils.getInstance().getKSession().insert(object);
+		DroolsUtils.getInstance().getKSession().fireAllRules();
 	}
 
 	public static void main(String[] args) {
@@ -78,7 +101,7 @@ public class Solver {
 			DroolsUtils.getInstance().clear();
 			Grid grid = new Grid(new Configuration(16, 16, 40));
 			Solver solver = new Solver(grid);
-			solver.solve();
+			solver.solve(false);
 			if (grid.getGameState() == GameState.WIN) {
 				win++;
 			} else {
--- src/main/java/it/unicam/cs/view/MainFrame.java
+++ src/main/java/it/unicam/cs/view/MainFrame.java
@@ -34,110 +34,18 @@ public class MainFrame extends JFrame {
 	private ButtonModel selectedDifficultyButtonModel = null;
 	private GameState gameState;
 	
-	public void createNewGameGUI() {
-		MainPanel panel = new MainPanel();
-		panel.addMouseListener(new MouseAdapter() {
-			private boolean mousePressed = false;
-			private Location squareLocation = null;
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				if (MainFrame.this.gameState != GameState.ONGOING) {
-					return;
-				}
-				if (SwingUtilities.isLeftMouseButton(e) || SwingUtilities.isRightMouseButton(e)) {
-					mousePressed = true;
-					squareLocation = panel.getSquareLocation(e.getPoint());
-				}
-			}
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				if (MainFrame.this.gameState != GameState.ONGOING) {
-					return;
-				}
-				if (!mousePressed) {
-					return;
-				}
-				if (SwingUtilities.isLeftMouseButton(e) || SwingUtilities.isRightMouseButton(e)) {
-					if (panel.getSquareLocation(e.getPoint()).equals(squareLocation)) {
-						if (!MainFrame.this.grid.isPopulated()) {
-							MainFrame.this.grid.populateSafeGrid(squareLocation);
-						}
-						if (SwingUtilities.isLeftMouseButton(e)) {
-							if (e.getClickCount() == 1) {
-								DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "UNCOVER" ).setFocus();
-							} else if (e.getClickCount() == 2) {
-								DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "CHORD" ).setFocus();
-							}
-						}
-						if (SwingUtilities.isRightMouseButton(e)) {
-							DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "FLAG" ).setFocus();
-						}
-						insertAndFire(squareLocation);
-						MainFrame.this.gameState = MainFrame.this.grid.getGameState();
-						if (MainFrame.this.gameState == GameState.LOSS) {
-							DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "LOSS" ).setFocus();
-							DroolsUtils.getInstance().getKSession().fireAllRules();
-						}
-						if (MainFrame.this.gameState == GameState.WIN) {
-							DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "WIN" ).setFocus();
-							DroolsUtils.getInstance().getKSession().fireAllRules();
-						}
-						panel.repaint();
-						System.out.println(MainFrame.this.grid);
-						System.out.println(DroolsUtils.getInstance().getKSession().getFactCount());
-					}
-				}
-				mousePressed = false;
-				squareLocation = null;
-			}
-		});
-		this.panel = panel;
-	}
-	
-	private synchronized void insertAndFire(Object object) {
-		DroolsUtils.getInstance().getKSession().insert(object);
-		DroolsUtils.getInstance().getKSession().fireAllRules();
-	}
-	
-	private boolean checkConfigurationValidity(Configuration configuration) {
-		if (configuration.getN_ROWS() <= 0 || configuration.getN_COLUMNS() <= 0 || configuration.getN_BOMBS() <= 0) {
-			return false;
-		}
-		if (configuration.getN_BOMBS() >= configuration.getN_ROWS() * configuration.getN_COLUMNS()) {
-			return false;
-		}
-		return true;
-	}
-	
-	private Dimension determinePreferredDimension() {
-		Dimension preferredDimension = new Dimension(grid.getConfig().getN_COLUMNS()*32, grid.getConfig().getN_ROWS()*32);
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		if (preferredDimension.width > screenSize.width || preferredDimension.height > screenSize.height) {
-			preferredDimension = new Dimension(screenSize.width, screenSize.height);
-		}
-		return preferredDimension;
-	}
-	
-	private void newGame(Grid grid) {
-	    DroolsUtils.getInstance().clear();
-		this.grid = grid;
-		this.gameState = GameState.ONGOING;
-		this.panel.setPreferredSize(determinePreferredDimension());
-		this.pack();
-		this.panel.init(grid);
-	}
-
 	public MainFrame(String title, Grid grid) {
 		super(title);
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(true);
 		this.setLocationRelativeTo(null);
-		
+
 		JMenuBar menuBar = new JMenuBar();
+		
         JMenu gameMenu = new JMenu("Game");
+        
+        // New game button
         JMenuItem newGameMenuItem = new JMenuItem("New Game");
         newGameMenuItem.addActionListener(new ActionListener() {
 
@@ -150,7 +58,8 @@ public class MainFrame extends JFrame {
 		});
         gameMenu.add(newGameMenuItem);
         gameMenu.addSeparator();
-
+        
+        // Beginner/Intermediate/Expert button group
         ButtonGroup buttonGroup = new ButtonGroup();
         for (Difficulty difficulty : Difficulty.values()) {
         	JCheckBoxMenuItem newDifficultyGameMenuItem = new JCheckBoxMenuItem(difficulty.toString().substring(0, 1).toUpperCase() + difficulty.toString().substring(1).toLowerCase());
@@ -171,7 +80,8 @@ public class MainFrame extends JFrame {
 			}
             gameMenu.add(newDifficultyGameMenuItem);
         }
-
+        
+        // Custom Configuration button
         JCheckBoxMenuItem customDifficultyGameMenuItem = new JCheckBoxMenuItem("Custom...");
         customDifficultyGameMenuItem.addActionListener(new ActionListener() {
 
@@ -215,29 +125,28 @@ public class MainFrame extends JFrame {
         menuBar.add(gameMenu);
 
         JMenu solverMenu = new JMenu("Solver");
+        // Solve complete button
         JMenuItem solveMenuItem = new JMenuItem("Solve");
         solveMenuItem.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Solver solver = new Solver(MainFrame.this.grid);
-				solver.solve();
-				MainFrame.this.gameState = MainFrame.this.grid.getGameState();
-				if (MainFrame.this.gameState == GameState.LOSS) {
-					DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "LOSS" ).setFocus();
-					DroolsUtils.getInstance().getKSession().fireAllRules();
-				}
-				if (MainFrame.this.gameState == GameState.WIN) {
-					DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "WIN" ).setFocus();
-					DroolsUtils.getInstance().getKSession().fireAllRules();
-				}
-				panel.repaint();
+				solve(false);
 			}
 		});
-        
+        // Solve by step button
+        JMenuItem solveByStepMenuItem = new JMenuItem("Solve by step");
+        solveByStepMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				solve(true);
+			}
+		});        
         solverMenu.add(solveMenuItem);
-        menuBar.add(solverMenu);
+        solverMenu.add(solveByStepMenuItem);
         
+        menuBar.add(solverMenu);
         this.setJMenuBar(menuBar);
 
         createNewGameGUI();
@@ -246,5 +155,145 @@ public class MainFrame extends JFrame {
 		this.getContentPane().add(this.panel, null);
 		this.setVisible(true);
 		this.pack();
+	}
+	
+	/**
+	 * Method to create the MainPanel representing the grid.
+	 */
+	private void createNewGameGUI() {
+		MainPanel panel = new MainPanel();
+		panel.addMouseListener(new MouseAdapter() {
+			private boolean mousePressed = false;
+			private Location squareLocation = null;
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				if (MainFrame.this.gameState != GameState.ONGOING) {
+					return; // exit if WIN or LOSS
+				}
+				if (SwingUtilities.isLeftMouseButton(e) || SwingUtilities.isRightMouseButton(e)) {
+					mousePressed = true; // if L or R is pressed, save pressed location
+					squareLocation = panel.getSquareLocation(e.getPoint());
+				}
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if (MainFrame.this.gameState != GameState.ONGOING) {
+					return; // exit if WIN or LOSS
+				}
+				if (!mousePressed) {
+					return; // exit if mouse is not pressed
+				}
+				if (SwingUtilities.isLeftMouseButton(e) || SwingUtilities.isRightMouseButton(e)) { // if L or R click
+					if (panel.getSquareLocation(e.getPoint()).equals(squareLocation)) { // if released location == pressed location
+						if (!MainFrame.this.grid.isPopulated()) { // populate grid if not
+							MainFrame.this.grid.populateSafeGrid(squareLocation);
+						}
+						if (SwingUtilities.isLeftMouseButton(e)) {
+							if (e.getClickCount() == 1) { // if 1 left click, activate UNCOVER rules
+								DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "UNCOVER" ).setFocus();
+							} else if (e.getClickCount() == 2) { // if 2 left click, activate CHORD rules
+								DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "CHORD" ).setFocus();
+							}
+						}
+						if (SwingUtilities.isRightMouseButton(e)) { // if right click, activate FLAG rules
+							DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "FLAG" ).setFocus();
+						}
+						insertAndFire(squareLocation);
+						MainFrame.this.gameState = MainFrame.this.grid.getGameState(); // update state
+						fireWinLossRules();
+						panel.repaint();
+						System.out.println(MainFrame.this.grid);
+						System.out.println(DroolsUtils.getInstance().getKSession().getFactCount());
+					}
+				}
+				mousePressed = false;
+				squareLocation = null;
+			}
+		});
+		this.panel = panel;
+	}
+	
+	/**
+	 * Thread-safe Method to insert an object into the working memory and fire all related rules.
+	 * 
+	 * @param object Object to insert into the working memory
+	 */
+	private synchronized void insertAndFire(Object object) {
+		DroolsUtils.getInstance().getKSession().insert(object);
+		DroolsUtils.getInstance().getKSession().fireAllRules();
+	}
+	
+	/**
+	 * Method to check if a certain Configuration is valid or not.
+	 * 
+	 * @param configuration The configuration to check.
+	 * @return True if the configuration is valid, False otherwise.
+	 */
+	private boolean checkConfigurationValidity(Configuration configuration) {
+		if (configuration.getN_ROWS() <= 0 || configuration.getN_COLUMNS() <= 0 || configuration.getN_BOMBS() <= 0) {
+			return false;
+		}
+		if (configuration.getN_BOMBS() >= configuration.getN_ROWS() * configuration.getN_COLUMNS()) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Method to obtain the preferred dimension for the MainPanel.
+	 * 
+	 * @return The preferred Dimension.
+	 */
+	private Dimension determinePreferredDimension() {
+		Dimension preferredDimension = new Dimension(grid.getConfig().getN_COLUMNS()*32, grid.getConfig().getN_ROWS()*32);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		if (preferredDimension.width > screenSize.width || preferredDimension.height > screenSize.height) {
+			preferredDimension = new Dimension(screenSize.width, screenSize.height);
+		}
+		return preferredDimension;
+	}
+	
+	/**
+	 * Method used to create/reset a game.
+	 * 
+	 * @param grid The grid used for the game.
+	 */
+	private void newGame(Grid grid) {
+	    DroolsUtils.getInstance().clear();
+		this.grid = grid;
+		this.gameState = GameState.ONGOING;
+		this.panel.setPreferredSize(determinePreferredDimension());
+		this.pack();
+		this.panel.init(grid);
+	}
+	
+
+    /**
+     * Method to solve the Minesweeper game using a Single Point Solver and the current state of the grid.
+     * 
+     * @param isByStep True if the Solver should perform only one resolution step, False otherwise.
+     */
+    private void solve(boolean isByStep) {
+		Solver solver = new Solver(MainFrame.this.grid);
+		solver.solve(isByStep);
+		MainFrame.this.gameState = MainFrame.this.grid.getGameState();
+		fireWinLossRules();
+		panel.repaint();
+	}
+
+	/**
+	 * Method to fire the corresponding rules in case of victory or defeat.
+	 */
+	private void fireWinLossRules() {
+		if (MainFrame.this.gameState == GameState.LOSS) {
+			DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "LOSS" ).setFocus();
+			DroolsUtils.getInstance().getKSession().fireAllRules();
+		}
+		if (MainFrame.this.gameState == GameState.WIN) {
+			DroolsUtils.getInstance().getKSession().getAgenda().getAgendaGroup( "WIN" ).setFocus();
+			DroolsUtils.getInstance().getKSession().fireAllRules();
+		}
 	}
 }
