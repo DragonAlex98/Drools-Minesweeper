a5796bd195e2a13af1229cea3488eed5343b8c3a
DragonAlex98 <alessandro.antinori@studenti.unicam.it>
2021-04-02 19:00:27 +0200

    Try all partitions with CSPSolver2

--- src/main/java/it/unicam/cs/CSPSolver2.java
+++ src/main/java/it/unicam/cs/CSPSolver2.java
@@ -63,90 +63,98 @@ public class CSPSolver2 implements MinesweeperSolver {
 
 	@Override
 	public SolveStep solveByStep() {
-		SolveStep singlePointStep = new SinglePointSolver(grid).solveByStep();
+		/*SolveStep singlePointStep = new SinglePointSolver(grid).solveByStep();
 		if (singlePointStep != null) {
 			return singlePointStep;
-		}
-		HashMap<Location, BoolVar> map = new HashMap<Location, BoolVar>();
-		Model model = new Model("Problemone");
-
+		}*/
+		
 		List<Set<Location>> partitions = calculatePartitions();
-		System.out.println(partitions.size());
 		if (partitions.size() == 0) {
 			return null;
 		}
-		Set<Location> biggestPartition = partitions.stream().sorted((x, y) -> Integer.compare(y.size(), x.size())).findFirst().get();
-		System.out.println(biggestPartition.size());
 		
-		biggestPartition.forEach(n -> {
-			String varName = String.format("(%d,%d)", n.getRow(), n.getColumn());
-			if (!map.containsKey(n)) {
-				BoolVar boolVar = model.boolVar(varName);
-				map.put(n, boolVar);
-			}
-		});
+		Set<Location> locationsToFlag = new HashSet<Location>();
+		Set<Location> locationsToUncover = new HashSet<Location>();
 		
-		if (map.size() == 0) {
-			return null;
-		}
+		partitions.stream().sorted((x, y) -> Integer.compare(y.size(), x.size())).forEach(p -> {
+			HashMap<Location, BoolVar> map = new HashMap<Location, BoolVar>();
+			Model model = new Model("Problemone");
 
-		grid.getGridAsStream()
-				.filter(s -> s.getType() == SquareType.NUMBER && s.getState() == SquareState.UNCOVERED)
-				.filter(s -> grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.COVERED).allMatch(n -> biggestPartition.contains(n.getLocation())))
-				.forEach(s -> {
-					List<BoolVar> boolVars = grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.COVERED).map(n -> map.get(n.getLocation())).collect(Collectors.toList());
-					if (boolVars.size() == 0) {
-						return;
-					}
-					int flaggedNeighbors = (int)grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.FLAGGED).count();
-					int sum = ((Number)s).getNeighbourBombsCount() - flaggedNeighbors;
-					BoolVar[] vars = boolVars.toArray(new BoolVar[] {});
-					model.sum(vars, "=", sum).post();
-		});
-
-		Map<Location, Integer> mySolution = null;
-		Solver chocoSolver = model.getSolver();
-		//System.out.println(chocoSolver);
-		//int count = 0;
-		while (chocoSolver.solve()) {
-			/*count++;
-			if (count == 20000) {
-				// STOP after 20000 solutions found
-				return null;
-			}*/
-			Solution solution = new Solution(model, map.values().toArray(new BoolVar[] {}));
-			solution.record();
-
-			if (mySolution == null) {
-				// always save the first solution
-				mySolution = new HashMap<Location, Integer>();
-				for (BoolVar bv : solution.retrieveBoolVars()) {
-					mySolution.put(getLocationFromString(bv.getName()), bv.getValue());
+			p.forEach(n -> {
+				String varName = String.format("(%d,%d)", n.getRow(), n.getColumn());
+				if (!map.containsKey(n)) {
+					BoolVar boolVar = model.boolVar(varName);
+					map.put(n, boolVar);
 				}
-			} else {
-				if (mySolution.size() == 0) {
-					// if no variables are always equal, then there is certain solution
+			});
+			
+			if (map.size() == 0) {
+				return;
+			}
+	
+			grid.getGridAsStream()
+					.filter(s -> s.getType() == SquareType.NUMBER && s.getState() == SquareState.UNCOVERED)
+					.filter(s -> grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.COVERED).allMatch(n -> p.contains(n.getLocation())))
+					.forEach(s -> {
+						List<BoolVar> boolVars = grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.COVERED).map(n -> map.get(n.getLocation())).collect(Collectors.toList());
+						if (boolVars.size() == 0) {
+							return;
+						}
+						int flaggedNeighbors = (int)grid.getNeighboursAsStream(s.getLocation()).filter(n -> n.getState() == SquareState.FLAGGED).count();
+						int sum = ((Number)s).getNeighbourBombsCount() - flaggedNeighbors;
+						BoolVar[] vars = boolVars.toArray(new BoolVar[] {});
+						model.sum(vars, "=", sum).post();
+			});
+	
+			Map<Location, Integer> mySolution = null;
+			Solver chocoSolver = model.getSolver();
+			//System.out.println(chocoSolver);
+			//int count = 0;
+			while (chocoSolver.solve()) {
+				/*count++;
+				if (count == 20000) {
+					// STOP after 20000 solutions found
 					return null;
-				} else {
+				}*/
+				Solution solution = new Solution(model, map.values().toArray(new BoolVar[] {}));
+				solution.record();
+	
+				if (mySolution == null) {
+					// always save the first solution
+					mySolution = new HashMap<Location, Integer>();
 					for (BoolVar bv : solution.retrieveBoolVars()) {
-						// for each new solution found, only keep the variables that remain the same
-						Location location = getLocationFromString(bv.getName());
-						if (mySolution.containsKey(location)) {
-							if (!mySolution.get(location).equals(bv.getValue())) {
-								mySolution.remove(location);
+						mySolution.put(getLocationFromString(bv.getName()), bv.getValue());
+					}
+				} else {
+					if (mySolution.size() == 0) {
+						// if no variables are always equal, then there is certain solution
+						return;
+					} else {
+						for (BoolVar bv : solution.retrieveBoolVars()) {
+							// for each new solution found, only keep the variables that remain the same
+							Location location = getLocationFromString(bv.getName());
+							if (mySolution.containsKey(location)) {
+								if (!mySolution.get(location).equals(bv.getValue())) {
+									mySolution.remove(location);
+								}
 							}
 						}
 					}
 				}
 			}
-		}
-		// no solution has been found
-		if (mySolution == null || mySolution.size() == 0) {
+			// no solution has been found
+			if (mySolution == null || mySolution.size() == 0) {
+				return;
+			}
+			
+			locationsToFlag.addAll(mySolution.entrySet().stream().filter(e -> e.getValue() == 1).map(e -> e.getKey()).collect(Collectors.toList()));
+			locationsToUncover.addAll(mySolution.entrySet().stream().filter(e -> e.getValue() == 0).map(e -> e.getKey()).collect(Collectors.toList()));
+		});
+		
+		if (locationsToFlag.isEmpty() && locationsToUncover.isEmpty()) {
 			return null;
 		}
-		
-		List<Location> locationsToFlag = mySolution.entrySet().stream().filter(e -> e.getValue() == 1).map(e -> e.getKey()).collect(Collectors.toList());
-		List<Location> locationsToUncover = mySolution.entrySet().stream().filter(e -> e.getValue() == 0).map(e -> e.getKey()).collect(Collectors.toList());
-		return new SolveStep(locationsToFlag, locationsToUncover);
+
+		return new SolveStep(new ArrayList<Location>(locationsToFlag), new ArrayList<Location>(locationsToUncover));
 	}
 }
