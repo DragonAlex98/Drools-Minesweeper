0191bcf6e4096ea47b57d39306f124991371c4d1
DragonAlex98 <alessandro.antinori@studenti.unicam.it>
2021-05-03 16:28:19 +0200

    Fixed frame location after newGame

--- src/main/java/it/unicam/cs/view/MainFrame.java
+++ src/main/java/it/unicam/cs/view/MainFrame.java
@@ -696,7 +696,7 @@ public class MainFrame extends JFrame {
 		this.grid = grid;
 		this.panel.setPreferredSize(determinePreferredDimension());
 		this.pack();
-		this.setLocationRelativeTo(null);
+		this.setLocationRelativeTo(this);
 		this.panel.init(grid);
 		this.repaint();
 	}
